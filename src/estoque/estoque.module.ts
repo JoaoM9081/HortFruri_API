4b5cb3b124b1d69b2ec2d918186d07ccf3c9630e
Joao Marcos Azevedo Cruz <joao.acruz@dataprev.gov.br>
2025-04-28 18:53:09 -0300

    .

--- src/estoque/estoque.module.ts
+++ src/estoque/estoque.module.ts
@@ -3,10 +3,12 @@ import { EstoqueService } from './estoque.service';
 import { EstoqueController } from './estoque.controller';
 import { Estoque } from './entities/estoque.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProdutoService } from 'src/produto/produto.service';
+import { LojaService } from 'src/loja/loja.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Estoque])],
   controllers: [EstoqueController],
-  providers: [EstoqueService],
+  providers: [EstoqueService, ProdutoService, LojaService],
 })
 export class EstoqueModule {}
--- src/produto/produto.module.ts
+++ src/produto/produto.module.ts
@@ -3,9 +3,10 @@ import { ProdutoService } from './produto.service';
 import { ProdutoController } from './produto.controller';
 import { Produto } from './entities/produto.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Estoque } from 'src/estoque/entities/estoque.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Produto])],
+  imports: [TypeOrmModule.forFeature([Estoque]), ProdutoModule],
   controllers: [ProdutoController],
   providers: [ProdutoService],
 })
